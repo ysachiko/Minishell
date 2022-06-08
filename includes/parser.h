9771dfd9152254194253ebca7cd84ab935e21072
Yurii Kovalev <71353141+ysachiko@users.noreply.github.com>
2022-06-08 16:39:57 +0300

    Add files via upload

--- includes/parser.h
+++ includes/parser.h
@@ -6,7 +6,7 @@
 /*   By: ysachiko <ysachiko@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/01 16:36:19 by ysachiko          #+#    #+#             */
-/*   Updated: 2022/06/08 14:12:59 by kezekiel         ###   ########.fr       */
+/*   Updated: 2022/06/08 14:28:23 by ysachiko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,8 +56,12 @@ typedef struct s_main
 	int		free_quote_flag;
 	int		in_double_quots;
 	int		in_single_quots;
+	t_hash	*hash_head;
+	t_hash	*tmp;
 	t_env	*env_list;
 }	t_main;
+
+
 
 void	parser(t_main *main);
 /*
@@ -70,23 +74,22 @@ void	ft_lstadd_back_hash(t_hash **lst, t_hash *new);
  USAGE FUNCTIONS
 */
 int		is_space(char c);
-int 	is_single_quote(char c);
+int		is_single_quote(char c);
 int		is_double_quote(char c);
 char	*str_realloc(char *str);
 void	str_add_new_symbol(char *str, char symbol);
 /*
 DEBUG_SHIT
 */
-void	DEBUG_PRINT_LIST(t_hash *head);
-void	DEBUG_PRINT_LIST_2(t_env *head);
+void	debug_print_list(t_hash *head);
+void	debug_print_list_2(t_env *head);
 /*
 LEXER_MAIN_USAGE
 */
 void	scip_space(t_main *main, int counter);
 int		extend_string(char **argument, char symbol, int counter);
 int		is_builtin(char *str);
-int		parse_lexer_list(t_main *main, t_hash *head);
-int		is_spec_symbol(char c);
+int		parse_lexer_list(t_hash *head);
 /*
 ENV_INIT
 */
@@ -99,19 +102,37 @@ t_env	*find_key_node(char *str, t_env *head);
 PARSE_ENV
 */
 int		parse_env(t_main *main, t_hash *head);
-void	env_str_refactor(t_main *main,t_hash *hash);
+void	env_str_refactor(t_main *main, t_hash *hash);
 int		str_refactor(t_main *main, t_hash *hash, int i);
-void	arg_str_refactor(t_main *main, t_hash *hash, int i);
+int		arg_str_refactor(t_main *main, t_hash *hash, int i);
 char	*find_env(char *str, int i);
 /*
 PARSE ENV USAGE
 */
 int		env_arg_len(char *str, int i);
+void	str_env_pars(t_main *main, char **str);
+void	set_empty_arg(t_hash *hash, int i, char *arg);
+char	*get_str_after_arg(t_hash *hash, int i, char *arg);
+char	*get_str_before_arg(t_hash *hash, int i);
+char	*find_env(char *str, int i);
+/*
+PARSER USAGE
+*/
+int		is_spec_symbol(char c);
 void	str_delete_symbol(t_hash *hash, int i);
+/*
+QUOTS
+*/
+char	*take_single_quote_arg(t_main *main, int counter);
+char	*return_double_quotes(t_main *main, int counter);
+char	*take_double_quote_args(t_main *main, int counter);
+void	refactor_double_quote_arg(t_main *main, int counter, char **argument);
+void	refactor_single_quote_arg(t_main *main, int counter, char **argument);
 /*
 FREE
 */
 void	free_split(char **split);
 void	clean_env(t_env **envp);
+
 
 #endif
